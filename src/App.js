a0c07c17a7e1976a0fbd5975efb4e35b42933177
Vineel Pynam <90417493+vineel-pynam@users.noreply.github.com>
2022-08-04 17:07:15 +0530

    Added Navbar and AddEmployee Pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
+import AddEmployee from './components/AddEmployee';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <>
+      <Navbar />
+      <AddEmployee />
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+ 
+const Navbar = () => {
+  return (
+  <div className='bg-gray-800'>
+    <div className='h-16 px-8 flex items-center'>
+      <p className='text-white front-bold'>
+        Employee Management System
+      </p>
+    </div>
+  </div>
+  );
+}
+
+export default Navbar
